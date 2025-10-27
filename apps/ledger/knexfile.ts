52ada5103b1327cd4e4d5c142a56d297a379906b
jitendrakumarpalei <jitendrakumarpalei6@gmail.com>
2025-10-27 21:46:16 +0530

    push the code

--- apps/ledger/knexfile.ts
+++ apps/ledger/knexfile.ts
@@ -4,4 +4,5 @@ const databaseUrl = process.env.DATABASE_URL ?? 'postgres://127.0.0.1:5432/ledge
 
 export default {
   development: buildKnexConfig(databaseUrl),
+  production: buildKnexConfig(databaseUrl),
 };
